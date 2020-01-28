89bac48c268848bdac0bb3d1a590cce277aee207
Erik Fábry <erik.fabry@smeonline.sk>
2020-01-28 19:45:56 +0100

    init commit of emf-web-crawler python package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='emf-web-crawler',  
+     version='0.1',
+     scripts=['emf-web-crawler'] ,
+     author="Erik Fabry",
+     author_email="erikfabry@gmail.com",
+     description="Basic web crawler",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/Max1mum4444/emf-web-crawler",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
