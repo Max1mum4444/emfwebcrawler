1cf016c73f214a438c9b2ea9144b6edda3fa232a
Erik Fábry <erik.fabry@smeonline.sk>
2020-01-28 20:02:36 +0100

    increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='emf-web-crawler',
-    version='0.1',
+    version='0.1.1',
     scripts=['emf-web-crawler'],
     author="Erik Fabry",
     author_email="erikfabry@gmail.com",
